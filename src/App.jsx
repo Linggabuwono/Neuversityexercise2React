540c886da16976e7f3a56fbefd7049b5b381009f
Lingga Buwono Jati <linggabuwonojati@gmail.com>
2023-03-03 19:17:45 +0700

    membuat file home, contact, pricing, service

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,47 @@
-import logo from "./logo.svg";
+// import logo from "./logo.svg";
 import "./App.css";
+import cat from "./images/cat.jpg";
+import Profile from "./components/Profile";
 import Tombol from "./components/Tombol";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <>
-      <Tombol title="Save"/>
-      <Tombol title="Reset"/>
-      <Tombol title="Delete"/>
+      <div>
+        <Navbar />
+      </div>
+      <div class="flex mx-auto justify-center">
+        <Profile
+          images={cat}
+          nama=" Tian"
+          umur="22"
+          alamat="Bantul Yogyakarta"
+          tinggi="180"
+          berat="90"
+        />
+        <Profile
+          images={cat}
+          nama=" Lingga"
+          umur="22"
+          alamat="Bantul Yogyakarta"
+          tinggi="180"
+          berat="90"
+        />
+        <Profile
+          images={cat}
+          nama=" Reza"
+          umur="22"
+          alamat="Bantul Yogyakarta"
+          tinggi="180"
+          berat="90"
+        />
+      </div>
+      <div class="text-center">
+        <Tombol title="Save" color="blue" />
+        <Tombol title="Reset" color="neutral" />
+        <Tombol title="Delete" color="red" />
+      </div>
     </>
   );
 }
--- src/components/Tombol.jsx
+++ src/components/Tombol.jsx
@@ -1,9 +1,12 @@
-function Tombol({title}) {
-    return (
-        <>
-        <button>{title}</button>
-        </>
-    );
-}
+function Tombol({ title, color}) {
+  return (
+    <>
+      <button
+        className={`bg-${color}-600 rounded-md text-white font-medium px-3 py-2 mx-5 mt-5`}>
+        {title}
+      </button>
+    </>
+  );
+};
 
 export default Tombol;
